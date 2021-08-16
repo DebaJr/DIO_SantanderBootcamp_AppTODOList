4bf8c679d771cc6ccc5cc9ba9be8ed6722a9816f
DebaJr <debastianijunior@gmail.com>
2021-08-16 18:50:32 -0300

    Added time picking and time formatting for new task/event. Altered texts related to tasks to "tasks and events". Added functionality (close activity) to navigation button (<) on toolbar.

--- app/src/main/java/com/debajr/todolist/ui/NewTaskActivity.kt
+++ app/src/main/java/com/debajr/todolist/ui/NewTaskActivity.kt
@@ -1,12 +1,16 @@
 package com.debajr.todolist.ui
 
 import android.os.Bundle
+import android.text.format.DateFormat.is24HourFormat
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.debajr.todolist.databinding.ActivityNewTaskBinding
 import com.debajr.todolist.extensions.formatToStringPTBR
 import com.debajr.todolist.extensions.text
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import kotlinx.android.synthetic.main.activity_new_task.*
 import java.util.*
 
 class NewTaskActivity : AppCompatActivity() {
@@ -29,6 +33,22 @@ class NewTaskActivity : AppCompatActivity() {
                 binding.tilDate.text = Date(it + timeZoneOffset).formatToStringPTBR()
             }
             datePicker.show(supportFragmentManager, "DATE_PICKER_TAG")
+        }
+
+        binding.tilTime.editText?.setOnClickListener {
+            val timePicker = MaterialTimePicker.Builder()
+                .setTimeFormat(if (is24HourFormat(this)) TimeFormat.CLOCK_24H else TimeFormat.CLOCK_12H )
+                .build()
+            timePicker.addOnPositiveButtonClickListener {
+                binding.tilTime.text = "${String.format("%02d", timePicker.hour)}:${String.format("%02d", timePicker.minute)}"
+            }
+            timePicker.show(supportFragmentManager, "TIME_PICKER_TAG")
+        }
+
+        binding.toolbar.setNavigationOnClickListener { finish() }
+
+        binding.efbConfirmCreateTask.setOnClickListener {
+            Log.e("CREATE TASK BTN", "Clicado no botão de confirmar a criação de nova tarefa / evento")
         }
     }
 }
